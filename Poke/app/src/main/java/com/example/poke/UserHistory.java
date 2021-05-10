604dfc43743c0fa1a09412f7fa208536d0cd2e3d
lcoo1 <varon12@naver.com>
2021-05-10 18:12:24 +0900

    Android Studio

--- Poke/app/src/main/java/com/example/poke/UserHistory.java
+++ Poke/app/src/main/java/com/example/poke/UserHistory.java
@@ -2,9 +2,19 @@ package com.example.poke;
 
 public class UserHistory {
     String recipeHistory;
+    int stars;
 
-    public UserHistory(String recipeHistory) {
+    public UserHistory(String recipeHistory, int stars) {
         this.recipeHistory = recipeHistory;
+        this.stars = stars;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
     }
 
     public void setRecipeHistory(String recipeHistory) {
