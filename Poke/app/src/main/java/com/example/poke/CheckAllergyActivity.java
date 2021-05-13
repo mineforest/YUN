2621e8c768661c960493f4391760a33431abaa83
lcoo1 <varon12@naver.com>
2021-05-13 17:11:34 +0900

    Poke

--- Poke/app/src/main/java/com/example/poke/CheckAllergyActivity.java
+++ Poke/app/src/main/java/com/example/poke/CheckAllergyActivity.java
@@ -1,27 +1,34 @@
 package com.example.poke;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
+
 
 public class CheckAllergyActivity extends AppCompatActivity {
     private static final String Tag = "CheckAllergyActivity";
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
-    private TextView textView;
+    private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
+    private TextView textView4;
+
+    ArrayList<TextView> textList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,28 +36,74 @@ public class CheckAllergyActivity extends AppCompatActivity {
         setContentView(R.layout.check_allergy);
         mAuth = FirebaseAuth.getInstance();
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.addValueEventListener(allergyListener);
-    }
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
 
-    ValueEventListener allergyListener = new ValueEventListener() {
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            String uid = user.getUid();
-            int i=1;
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        textView3 = (TextView)findViewById(R.id.textView3);
+        textView4 = (TextView)findViewById(R.id.textView4);
+        textList.add(textView1);
+        textList.add(textView2);
+        textList.add(textView3);
+        textList.add(textView4);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("preference").child(uid).addChildEventListener(allergyListener);
+        //mDatabase.addValueEventListener(allergyListener);
+    }
+//
+//    ValueEventListener allergyListener = new ValueEventListener() {
+//            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//            String uid = user.getUid();
+//            int i=0;
+//
+//        @Override
+//        public void onDataChange(DataSnapshot dataSnapshot) {
+//            for (DataSnapshot snapshot : dataSnapshot.child("preference").child(uid).getChildren()) {
+//                textList.get(i).setText(snapshot.getValue().toString());
+//                i++;
+//                }
+//        }
+//
+//        @Override
+//        public void onCancelled(DatabaseError databaseError) {
+//
+//        }
+//    };
+
+    ChildEventListener allergyListener = new ChildEventListener() {
+
+            int i=0;
         @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            for (DataSnapshot snapshot : dataSnapshot.child("users").child(uid).child("preference").getChildren()) {
-                Log.d(Tag,i+" "+snapshot.getValue().toString());
-                i++;
-                //((TextView)findViewById(R.id.textView+i)).setText(snapshot.getValue().toString());
-                }
+        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            UserAllergy allergy = snapshot.getValue(UserAllergy.class);
+            textList.get(i).setText(allergy.getPreference());
+            //textList.get(i).setText(snapshot.child("preference").child(uid).getChildren().toString());
+            i++;
+
         }
 
         @Override
-        public void onCancelled(DatabaseError databaseError) {
+        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
 
         }
     };
+
 }
 
 
--- Poke/app/src/main/java/com/example/poke/MyInfoActivity.java
+++ Poke/app/src/main/java/com/example/poke/MyInfoActivity.java
@@ -3,21 +3,53 @@ package com.example.poke;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MyInfoActivity extends AppCompatActivity {
+    TextView allergyCountTextView;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_info);
+        mAuth = FirebaseAuth.getInstance();
 
+        allergyCountTextView = findViewById(R.id.allergyCountTextView);
         findViewById(R.id.historyButton).setOnClickListener(onClickListener);
         findViewById(R.id.dibsButton).setOnClickListener(onClickListener);
         findViewById(R.id.allergyButton).setOnClickListener(onClickListener);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.addValueEventListener(allergyListener);
     }
+
+    ValueEventListener allergyListener = new ValueEventListener() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            allergyCountTextView.setText(Long.toString(dataSnapshot.child("preference").child(uid).getChildrenCount()));
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
 
     View.OnClickListener onClickListener = new View.OnClickListener(){
         @Override
