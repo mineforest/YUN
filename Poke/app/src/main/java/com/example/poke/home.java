5bd8ed8384f295638e846c52f68e938e19a5d2ba
lcoo1 <varon12@naver.com>
2021-05-26 15:37:47 +0900

    Poke

--- Poke/app/src/main/java/com/example/poke/home.java
+++ Poke/app/src/main/java/com/example/poke/home.java
@@ -18,12 +18,12 @@ public class home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.btm_nav);
 
         mBottomNavigationView=findViewById(R.id.bottom_navigation);
 
         //첫 화면 띄우기
-        getSupportFragmentManager().beginTransaction().add(R.id.frame_container,new Frag2()).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_container,new MainActivity()).commit();
 
         //case 함수를 통해 클릭 받을 때마다 화면 변경하기
         mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,7 +34,7 @@ public class home extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new Frag1()).commit();
                         break;
                     case R.id.nav_home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new Frag2()).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new MainActivity()).commit();
                         break;
                     case R.id.nav_ingredient:
                         getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new Frag3()).commit();
