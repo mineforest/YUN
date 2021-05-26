2e9bf7406c905c56390f206163d1ae3524bcd2aa
H-HS <81289075+H-HS@users.noreply.github.com>
2021-05-26 15:37:59 +0900

    Poke

--- Poke/app/src/main/java/com/example/poke/home.java
+++ Poke/app/src/main/java/com/example/poke/home.java
@@ -18,7 +18,7 @@ public class home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.btm_nav);
 
         mBottomNavigationView=findViewById(R.id.bottom_navigation);
 
