0a5036ba00e7d5d0f36ffe7ae07fc959b908b2ff
H-HS <81289075+H-HS@users.noreply.github.com>
2021-09-09 17:06:18 +0900

    search done

--- Poke/app/src/main/java/com/example/poke/SearchFragment.java
+++ Poke/app/src/main/java/com/example/poke/SearchFragment.java
@@ -2,10 +2,12 @@ package com.example.poke;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,24 +16,41 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.algolia.search.saas.AlgoliaException;
+import com.algolia.search.saas.Client;
+import com.algolia.search.saas.CompletionHandler;
+import com.algolia.search.saas.Index;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment implements TextWatcher {
+    private static final String TAG = "tag";
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private FirebaseDatabase database;
@@ -44,7 +63,6 @@ public class SearchFragment extends Fragment implements TextWatcher {
     String uid;
     ImageView clear_btn;
     ProgressDialog progressDialog;
-    Handler handler = new Handler();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,65 +72,55 @@ public class SearchFragment extends Fragment implements TextWatcher {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
         View view = inflater.inflate(R.layout.search, container, false);
-
-        progressDialog = new ProgressDialog(getActivity());
-
-        progressDialog.show();
-        progressDialog.setContentView(R.layout.progress_dialog);
-        progressDialog.getWindow().setBackgroundDrawableResource(
-                android.R.color.transparent
-        );
-        progressDialog.setCancelable(false);
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        String[] test_ids = {"1011256", "6867464", "6867464","6867464", "6867464", "6867464", "1166652",
-                                "6867464", "6867464", "6867464", "6867464", "6867464", "6867464"};
-                        FirebaseFirestore db = FirebaseFirestore.getInstance();
-                        for(int i =0;i<test_ids.length; i++) {
-                            DocumentReference docRef = db.collection("recipe").document(test_ids[i]);
-                            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                                @Override
-                                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                    String rcp_id = documentSnapshot.getData().get("id").toString();
-                                    String title = documentSnapshot.getData().get("name").toString();
-                                    String thumbnail = documentSnapshot.getData().get("thumbnail").toString();
-                                    Recipe_get r = new Recipe_get(rcp_id,thumbnail, title);
-                                    searchList.add(r);
-                                    adapter.notifyDataSetChanged();
-                                }
-                            });
-                        }
-
-                        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.search_rv);
-                        recyclerView.setHasFixedSize(true);
-                        //원래 위치
-//                        editText = (EditText) view.findViewById(R.id.rec_search);
-//                        editText.addTextChangedListener(this);
-                        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-                        adapter = new SearchAdapter(searchList);
-                        recyclerView.setAdapter(adapter);
-                        clear_btn = (ImageView) view.findViewById(R.id.clear_btn);
-                        clearText();
-
-                        new android.os.Handler().post(new Runnable() {
-                            @Override
-                            public void run() {
-                                progressDialog.dismiss();
-                            }
-                        });
-                    }
-                });
-            }
-        });
         editText = (EditText) view.findViewById(R.id.rec_search);
         editText.addTextChangedListener(this);
-        thread.start();
         ArrayList<Object> filteredList = new ArrayList<>();
+
+//        progressDialog = new ProgressDialog(getActivity());
+//
+//        progressDialog.show();
+//        progressDialog.setContentView(R.layout.progress_dialog);
+//        progressDialog.getWindow().setBackgroundDrawableResource(
+//                android.R.color.transparent
+//        );
+//        progressDialog.setCancelable(false);
+
+        new Thread() {
+            @Override
+            public void run() {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("recipe")
+                       // .whereEqualTo("name",editText.getText().toString())
+                        .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                Log.d("dadf",editText.getText().toString());
+                                if (error != null) {
+                                    Log.w(TAG, "Oh ~ no ~");
+                                    return;
+                                }
+
+                                for (QueryDocumentSnapshot doc : value) {
+                                    if(doc.getData().get("name").toString().contains(editText.getText().toString()))
+                                    {
+                                        String rcp_id = doc.getData().get("id").toString();
+                                        String title = doc.getData().get("name").toString();
+                                        String thumbnail = doc.getData().get("thumbnail").toString();
+                                        Recipe_get r = new Recipe_get(rcp_id, thumbnail, title);
+                                        searchList.add(r);
+                                        adapter.notifyDataSetChanged();
+                                    }
+                                }
+                            }
+                        });
+
+            }
+        }.start();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.search_rv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new SearchAdapter(searchList);
+        recyclerView.setAdapter(adapter);
 
         return view;
 
@@ -136,12 +144,6 @@ public class SearchFragment extends Fragment implements TextWatcher {
         searchFilter(searchText);
 
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
     public void searchFilter(String searchText) {
         searchedList.clear();
 
@@ -153,6 +155,12 @@ public class SearchFragment extends Fragment implements TextWatcher {
 
         adapter.filterList(searchedList);
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
     private void clearText() {
         clear_btn.setOnClickListener(new View.OnClickListener() {
             @Override
