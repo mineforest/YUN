d2d87e6e7ccb65b63f42fcaa9da40dd379bd3010
TechnoHong <tjrwns51@naver.com>
2021-10-28 22:41:29 +0900

    tts complete

--- Poke/app/src/main/java/com/example/poke/Recipe_step_Activity.java
+++ Poke/app/src/main/java/com/example/poke/Recipe_step_Activity.java
@@ -1,12 +1,17 @@
 package com.example.poke;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
@@ -14,6 +19,8 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class Recipe_step_Activity extends AppCompatActivity {
 
@@ -23,6 +30,10 @@ public class Recipe_step_Activity extends AppCompatActivity {
     private Button nextbutton;
     private ViewPager2 viewPager;
     private ArrayList<String> ingres;
+    private TextToSpeech tts;
+    private Boolean isTTS = false;
+    private ImageView ttsBtn;
+    private HashMap<String, String> map = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +46,9 @@ public class Recipe_step_Activity extends AppCompatActivity {
         springDotsIndicator = findViewById(R.id.dots_indicator);
         nextbutton = findViewById(R.id.nextButton);
         viewPager = findViewById(R.id.recipe_viewpager);
+        ttsBtn = findViewById(R.id.tts_btn);
 
+        ttsBtn.setOnClickListener(ttsButtonClickListener);
         nextbutton.setOnClickListener(nextButtonClickListener);
         viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
@@ -47,6 +60,33 @@ public class Recipe_step_Activity extends AppCompatActivity {
                 else {
                     nextbutton.setText("다음");
                 }
+            }
+        });
+
+        tts = new TextToSpeech(Recipe_step_Activity.this, status -> {
+            if(status!= TextToSpeech.ERROR) {
+                tts.setLanguage(Locale.KOREAN);
+                map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "pokepoke");
+
+                tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                    @Override
+                    public void onStart(String utteranceId) {
+                        ttsBtn.setImageResource(R.drawable.ic_pause_black_24dp);
+                        isTTS = true;
+                    }
+
+                    @Override
+                    public void onDone(String utteranceId) {
+                        int curr = viewPager.getCurrentItem();
+                        viewPager.setCurrentItem(curr+1,true);
+                        ttsBtn.setImageResource(R.drawable.ic_volume_up_black_24dp);
+                    }
+
+                    @Override
+                    public void onError(String utteranceId) {
+
+                    }
+                });
             }
         });
 
@@ -83,4 +123,29 @@ public class Recipe_step_Activity extends AppCompatActivity {
             else viewPager.setCurrentItem(curr+1,true);
         }
     };
+
+    View.OnClickListener ttsButtonClickListener = new View.OnClickListener() {
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onClick(View v) {
+            if(!isTTS ) {
+                String text = rcp.getRecipe().get(viewPager.getCurrentItem());
+                tts.setPitch(1.0f);
+                tts.setSpeechRate(1.0f);
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+            }
+            else {
+                tts.stop();
+                ttsBtn.setImageResource(R.drawable.ic_volume_up_black_24dp);
+                isTTS = false;
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        tts.stop();
+        tts.shutdown();
+        super.onDestroy();
+    }
 }
