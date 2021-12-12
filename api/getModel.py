0a72f796cf2731885fc823ce92295b1ad9eedfd0
mineforest <minefor47@gmail.com>
2021-12-12 20:40:09 +0900

    iwantmyownweekend

--- api/getModel.py
+++ api/getModel.py
@@ -2,17 +2,13 @@ from konlpy.tag import Okt
 from gensim.models import Word2Vec
 import gensim
 import pandas as pd
-#https://wikidocs.net/50739
 
-#fread = open('wiki_data.txt', encoding="utf8")
 okt=Okt()
 
-df = pd.read_csv('1001rcp.csv', encoding='utf-8')
+df = pd.read_csv('CSV파일명.csv', encoding='utf-8')
 
 def arrToStr(arr):
   return arr.str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]"," ")
-
-
 
 result = []
 for i,rcp in enumerate(df['recipe']):
@@ -28,21 +24,10 @@ for i,rcp in enumerate(df['recipe']):
         print("%d번째 While문."%i)
         print(temp)
     
-df.to_csv('1018rcp.csv', encoding='utf-8')
+df.to_csv('CSV파일명.csv', encoding='utf-8')
 print('총 샘플의 개수 : {}'.format(len(result)))
 
 word2vec_model = Word2Vec(vector_size=300, window=5, min_count=2, workers=-1)
 word2vec_model.build_vocab(result)
 word2vec_model.train(result, total_examples = word2vec_model.corpus_count, epochs=15)
-word2vec_model.save('1018w2v.model')
-
-
-
-"""
-df2['cleand'] = arrToStr(df2['ingre_name']) + ' ' + arrToStr(df2['tag'])
-
-df2['cleand'].replace('', np.nan, inplace=True)
-df2 = df2[df2['cleand'].notna()]
-print(len(df2))
-
-"""
+word2vec_model.save('Model명.model')
